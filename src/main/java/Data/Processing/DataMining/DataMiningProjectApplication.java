dc1fe0e2ed9f2e50710e80dcb9fa370f0b560328
Nguyen Minh Thong <Smart.Nguyen.0411@gmail.com>
2025-04-22 22:08:22 +0700

    22/4/2025

--- src/main/java/Data/Processing/DataMining/DataMiningProjectApplication.java
+++ src/main/java/Data/Processing/DataMining/DataMiningProjectApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import Data.Processing.DataMining.Entity.DatasetEntity;
 import Data.Processing.DataMining.Service.DataClassificationService;
 import Data.Processing.DataMining.Service.DataProcessingService;
+<<<<<<< Updated upstream
 import Data.Processing.DataMining.Service.DataTestingService;
+=======
+>>>>>>> Stashed changes
 
 @SpringBootApplication
 public class DataMiningProjectApplication {
 
 	public static void main(String[] args) throws Exception {
+<<<<<<< Updated upstream
 		DatasetEntity trainingEntity = new DatasetEntity("src\\main\\java\\Data\\Processing\\DataMining\\TestingFile\\ReutersGrain-train.arff");
 		DatasetEntity testingEntity = new DatasetEntity("src\\main\\java\\Data\\Processing\\DataMining\\TestingFile\\ReutersGrain-test.arff");
 
@@ -20,6 +24,15 @@ public class DataMiningProjectApplication {
 		//System.out.println(dataProcessingService.Normalize(trainingEntity.getDataset(), null).toSummaryString());
 		trainingEntity.setDataset(dataProcessingService.stringToNomial(trainingEntity.getDataset(), null));
 		System.out.println(dataTestingService.evaluation(dataProcessingService.stringToNomial(trainingEntity.getDataset(), null), dataProcessingService.stringToNomial(testingEntity.getDataset(), null), classificationService.BayesNet(dataProcessingService.stringToNomial(testingEntity.getDataset(), null), null)).toSummaryString());
+=======
+		DatasetEntity trainingEntity = new DatasetEntity("src\\main\\java\\Data\\Processing\\DataMining\\TestingFile\\cleaned_data.csv");
+		DatasetEntity testingEntity = new DatasetEntity("src\\main\\java\\Data\\Processing\\DataMining\\TestingFile\\cleaned_insight_data.csv");
+
+		DataProcessingService dataProcessingService = new DataProcessingService();
+		DataClassificationService classificationService = new DataClassificationService();
+		System.out.println(dataProcessingService.checkForDuplicates(trainingEntity.getDataset()));
+	
+>>>>>>> Stashed changes
 		SpringApplication.run(DataMiningProjectApplication.class, args);
 	}
 }
